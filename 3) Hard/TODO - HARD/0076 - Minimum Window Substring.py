f52e33844a003563e4669f8105a6259ee1c285a9
apop <amitpom14@gmail.com>
2024-09-19 10:53:10 +0300

    answered question num 0076

---
+++ 3) Hard/TODO - HARD/0076 - Minimum Window Substring.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "": return "" #~ Edge case
+        countT,window = {},{}
+        
+        for letterKey in t:              #* t is the requested chars
+            countT[letterKey]= 1 + countT.get(letterKey,0)
+        
+        have, need = 0,len(countT)
+        res,resLen = [-1,-1], float("infinity")
+        left = 0
+        for right in range(len(s)): #*
+            letter = s[right]
+            window[letter] = 1 + window.get(letter,0)
+            
+            if letter in countT and window[letter] == countT[letter]:
+                have +=1
+            
+            while have == need:
+                #! update our result
+                if (right-left +1)<resLen:
+                    res = [left,right]
+                    resLen = (right-left+1)
+                #! pop from the left of our window
+                window[s[left]]-=1
+                if s[left] in countT and window[s[left]] < countT[s[left]]:
+                    have -=1
+                left+=1
+        left,right = res
+        return s[left:right+1] if resLen != float("infinity") else ""
+
+
+#*-------Tests-------#
+sol = Solution()
+array1 = ["ADOBECODEBANC","ABC"]
+test1=sol.minWindow(array1[0],array1[1] )
+print(f"{test1}")
+
+#*-------------------#
+#^ Time Complexity:
+#^ 
+#^ Space Complexity: 
+#^ 
